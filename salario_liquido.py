fe36dcd456c37c5ff3961226d368192d93bdf148
Jonathan <90427766+JonathanAsf@users.noreply.github.com>
2023-10-04 10:47:07 -0300

    Update salario_liquido.py
    Atribuição opcional para o sindicato
    Melhor formatação para os valores monetários

--- salario_liquido.py
+++ salario_liquido.py
@@ -1,11 +1,17 @@
 print("Descubra o seu salário líquido")
-salario_bruto = float(input("Digite o seu salário bruto: "))
+salario_bruto = float(input("Digite o seu salário bruto: R$")) 
 desconto_IR = salario_bruto* 0.11
-print(f"O desconto do IR sobre o salario bruto é de: {desconto_IR}")
+print(f"O desconto do IR sobre o salario bruto é de: R${desconto_IR}")
 desconto_INSS = salario_bruto*0.08
-print(f"O desconto do INSS sobre o salario bruto é de:{desconto_INSS}")
-desconto_sindicato = salario_bruto*0.05
-print(f"O desconto sindical sobre o salario bruto é de: {desconto_sindicato}")
-
+print(f"O desconto do INSS sobre o salario bruto é de: R${desconto_INSS}")
+print("Você contribui para o sindicato? ")
+print("Digite 1 para SIM")
+print("Digite 2 para NÃO")
+sindicato = int(input(""))
+if sindicato == 1:
+    desconto_sindicato = salario_bruto*0.05
+    print(f"O desconto sindical sobre o salario bruto é de: R${desconto_sindicato}")
+else:
+    pass
 descontos = (desconto_INSS + desconto_IR + desconto_sindicato)
-print("O salário líquido é de: {}".format(salario_bruto-descontos))
+print("O salário líquido é de: R${}".format(salario_bruto-descontos))
