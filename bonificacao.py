479c524bd2e81fcf66e3681653f2aaad53353f25
Jonathan <90427766+JonathanAsf@users.noreply.github.com>
2023-10-04 11:02:55 -0300

    Create bonificacao.py
    Sistema de bonificação de acordo com o cumprimento da meta.
    A meta e o valor gerado nas vendas do funcionário devem ser declarados.
    Caso o funcionário tenha atingindo as metas, recebe um bônus de 10% do total vendido. Caso venda o dobro, a bonificação passa a ser de 15%.
    Se a meta não for alcançada não haverá bônus.

---
+++ bonificacao.py
@@ -0,0 +1,18 @@
+#Cálculo de bônus referente a meta de vendas.
+#Se o funcionário alcaçar a meta é pago 10% das vendas totais.
+#Se o funcionário alcançar o dobro da meta é pago 15% das vendas totais.
+#Se o funcionário não atingir a meta não haverá bônus
+
+meta = int(input("Digite o valor da meta de vendas: "))
+vendas_funcionario = int(input("Valor gerado nas vendas do funcionário: "))
+
+if vendas_funcionario >= meta:
+    print("O funcionário tem direito ao bônus!")
+    if vendas_funcionario >= (meta*2):  
+        print("O bônus do funcionário é de: R$ {} (15% sobre o valor total)".format(vendas_funcionario*0.15))
+        print("O total a receber é: {}".format((vendas_funcionario*0.15)+vendas_funcionario))
+    else :
+        print("O bônus do funcionário é de: R$ {}  (10% sobre o valor total)".format(vendas_funcionario*0.10))
+        print("O total a receber é: {}".format((vendas_funcionario*0.10)+vendas_funcionario))
+else:
+     print("O funcionário não teve bônus, pois suas vendas não alcançam a meta")
