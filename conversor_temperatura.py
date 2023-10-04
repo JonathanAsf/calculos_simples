f3ad3c56a79a142666e906ffd3118fabeee74721
Jonathan <90427766+JonathanAsf@users.noreply.github.com>
2023-10-04 10:06:10 -0300

    Update conversor_temperatura.py
    validação da opção. Caso a escolha não esteja nas opções a mensagem de invalidez da resposta aparece antes do cálculo

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -2,11 +2,14 @@ print("Bem vindo ao programa de conversão de unidades de temperaturas")
 print("Selecione a unidade de media inicial")
 print("Digite 1 para converter de GRAUS CELSIUS para GRAUS FAHRENHEIT")
 print("Digite 2 para converter de GRAUS FAHRENHEIT para GRAUS CELSIUS")
-opcao = int(input(" "))
-temperatura = float(input("Digite a temperatura que deseja converter"))            
+opcao = int(input(""))
+
+if opcao != 1 and 2:
+      print("Opção inválida")
+      
+temperatura = float(input("Digite a temperatura que deseja converter: "))   
+         
 if opcao == 1:
     print(f"{temperatura}°C é igual a {temperatura*1.8 + 32}°F") 
-elif opcao == 2:
-  print(f"{temperatura}°F é igual a {(temperatura - 32)/1.8}°C")
 else:
-  print("Opção inválida")
+  print(f"{temperatura}°F é igual a {(temperatura - 32)/1.8}°C")
