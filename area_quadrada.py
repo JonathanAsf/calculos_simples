1a1efcdf54f45d5ba99f3738acb690a603d4a9e3
Jonathan <90427766+JonathanAsf@users.noreply.github.com>
2023-10-04 20:06:34 -0300

    Create area_quadrada.py

---
+++ area_quadrada.py
@@ -0,0 +1,4 @@
+print("Descubra a área quadrada")
+altura = float(input("Digite a altura: "))
+base = float(input("Digite a base: "))
+print(f"De acordo com os dados, a área quadrada é: {base*altura}")
