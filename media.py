c0efb491f8148005fe4fbb4dcdab317602504067
Jonathan <90427766+JonathanAsf@users.noreply.github.com>
2023-10-06 09:02:01 -0300

    Update media.py
    adição de comentário "aprovado" ou "reprovado"

--- media.py
+++ media.py
@@ -3,4 +3,11 @@ nota_1 = int(input("Digite a primeira nota: "))
 nota_2 = int(input("Digite a segunda nota: "))
 nota_3 = int(input("Digite a terceira nota: "))
 nota_4 = int(input("Digite a quarta nota: "))
-print(f"A média das notas é: {(nota_1 + nota_2 + nota_3 + nota_4)/4}")
+media = (nota_1 + nota_2 + nota_3 + nota_4)/4
+if media <= 6:
+    print("O aluno foi reprovado")
+elif media >= 7:
+    print("O aluno foi aprovado")
+else:
+    print("Erro inesperado no cálculo da média")
+print(f"A média das notas é: {media}")
